37f0fa48b2ff6aac6e869b580de43df65d2593dc
IvanAvila123 <ivanavilar456@gmail.com>
2025-11-16 22:05:56 -0600

    se crea configuracion para smartShelf

--- app/Models/SmartShelf.php
+++ app/Models/SmartShelf.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SmartShelf extends Model
 {
+
+    protected $fillable = [
+        'id',
+        'store_id',
+        'product_id',
+        'hardware_id',
+        'status',
+        'current_weight',
+        'last_reported_at',
+    ];
+
     public function store(): BelongsTo
 {
     return $this->belongsTo(Store::class);
