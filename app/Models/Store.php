e8428a78a0a29caab0112a1aab50a2a3c7d77e09
IvanAvila123 <ivanavilar456@gmail.com>
2025-11-16 21:59:21 -0600

    se hace la configuracion del modelo store

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'name',
+        'logo_url',
+        'whatsapp_number',
+        'slug',
+        'mercado_pago_key',
+        'clabe_interbancaria',
+
+    ];
+
+    
 }
