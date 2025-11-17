47571a32d7b1981870c288f387a9c3ac9d29a509
IvanAvila123 <ivanavilar456@gmail.com>
2025-11-16 22:01:49 -0600

    se hace configuracion de product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    protected $fillable = [
+        'name',
+        'store_id',
+        'name',
+        'description',
+        'price',
+        'image_url',
+        'sku',
+        'stock_count',
+        'is_visible_on_quickorder',
+        'unit_weight_grams',
+        'low_stock_threshold',
+    ];
 }
