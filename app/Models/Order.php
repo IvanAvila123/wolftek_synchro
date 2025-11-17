48ea78d7199f23fb5786472c51095b2dd5cc8d4e
IvanAvila123 <ivanavilar456@gmail.com>
2025-11-16 22:08:17 -0600

    se crea configuracion de order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'store_id',
+        'customer_name',
+        'costumer_phone',
+        'status',
+        'total',
+        'payment_method',
+        'payment_status',
+    ];
 }
